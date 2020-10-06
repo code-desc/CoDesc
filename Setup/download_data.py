d0c25526be059074bcae522188340f81f7480cb2
code-description2020 <code.description.2020@gmail.com>
2020-10-07 02:20:12 +0600

    dataset links added

--- Setup/download_data.py
+++ Setup/download_data.py
@@ -8,23 +8,23 @@ os.chdir('data/')
 print('Starting download...')
 mega = Mega()
 
-mega.download_url('')  
+mega.download_url('https://mega.nz/file/Rsx3zaqY#stMEpCe33JLAIcslPZRf6sKXcka3JW3eivgjR5DFl5U')  
 print("CodeCorpus.7z downloaded")
 os.system("7z x CodeCorpus.7z")
 
-mega.download_url('')  
+mega.download_url('https://mega.nz/file/Q4oxQCIb#CT7P5zq1WbiWOLTeafg-mFD2QZEmF1YwZmDhGZkzU90')  
 print("csn_preprocessed_data.7z downloaded")
 os.system("7z x csn_preprocessed_data.7z")
 
-mega.download_url('')  
+mega.download_url('https://mega.nz/file/t543VCyA#4BLjB28yYNDT9kXBs6NWmY1ADOCMyuvqDXLg9yPhiaI')  
 print("csn_preprocessed_data_balanced_partition.7z downloaded")
 os.system("7z x csn_preprocessed_data_balanced_partition.7z")
 
-mega.download_url('')  
+mega.download_url('https://mega.nz/file/Npo1RaBZ#ug6jJPacpjNht537PmOwxsR2MlOps0Y-LOwhx8lQ5ys')  
 print("ncs_preprocessed_data.7z downloaded")
 os.system("7z x ncs_preprocessed_data.7z")
 
-mega.download_url('')  
+mega.download_url('https://mega.nz/file/ZpoBla5a#qeSxMXP6v-2FEI237cIVrFhuqnq5DHh88_EKuReSY3k')  
 print("original_data.7z downloaded")
 os.system("7z x original_data.7z")
 
